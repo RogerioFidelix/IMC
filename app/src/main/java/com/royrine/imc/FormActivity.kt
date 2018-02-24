a65be71b734172aa508d19440e29d426e931e676
logonrm <logonrm@fiap.com.br>
2018-02-23 23:02:52 -0300

    Troca de tela e passagem de Dados

--- app/src/main/java/com/royrine/imc/FormActivity.kt
+++ app/src/main/java/com/royrine/imc/FormActivity.kt
@@ -1,5 +1,6 @@
 package com.royrine.imc
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_form.*
@@ -14,6 +15,18 @@ class FormActivity : AppCompatActivity() {
             inputAltura.editText?.setText("")
             inputPeso.editText?.setText("")
             inputPeso.editText?.requestFocus()
+        }
+
+        btCalculate.setOnClickListener {
+            val telaResultado = Intent (this, ResultActivity::class.java)
+
+            telaResultado.putExtra("PESO",
+                    inputPeso.editText?.text.toString())
+
+            telaResultado.putExtra("ALTURA",
+                    inputAltura.editText?.text.toString())
+
+            startActivity(telaResultado)
         }
     }
 }
